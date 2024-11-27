17c1e602e5ab7203111b40de277352d8ce49b04f
udo-bit <enote_gxl@163.com>
2024-11-27 15:43:23 +0800

    typedef

--- demo1.dart
+++ demo1.dart
@@ -1,4 +1,18 @@
 void main() {
-  var list = List<String>.from(['1']);
-  print(list);
+  Animal cat = Cat("name", 1, 'orange');
+  print(cat.color); // orange
+}
+
+class Animal {
+  final String name;
+  final int age;
+  final String color;
+
+  Animal(this.name, this.age, this.color);
+}
+
+class Cat extends Animal {
+  final String color;
+
+  Cat(String name, int age, this.color) : super(name, age, color);
 }
