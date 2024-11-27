b69794b63fb6878eed1b633dd029d59388c98831
udo-bit <enote_gxl@163.com>
2024-11-27 15:03:12 +0800

    collection

--- demo1.dart
+++ demo1.dart
@@ -1,6 +1,10 @@
 void main() {
-  (int, int, {String name, int age}) v1;
-  v1 = (1, 2, name: "John", age: 30);
-  print(v1.name);
-  print(v1.$1);
+  var login = 'Manager1';
+  var nav = [
+    'Home',
+    'Furniture',
+    'Plants',
+    if (login case 'Manager') 'Inventory'
+  ];
+  print(nav);
 }
