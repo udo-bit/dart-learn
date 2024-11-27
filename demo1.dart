f4677b7316dbc3cce70ddbc2be36656506fa436b
udo-bit <enote_gxl@163.com>
2024-11-27 20:47:23 +0800

    pattern

--- demo1.dart
+++ demo1.dart
@@ -1,11 +1,8 @@
 void main() {
-  const number = 2;
-  switch (number) {
-    case 1:
-      print("number is 1");
-    case 2:
-      print("number is 2");
-    default:
-      print("number is not 1 or 2");
-  }
+  var (a, b) = userInfo();
+  print('name: $a, age: $b');
+}
+
+(String name, int age) userInfo() {
+  return ('Tom', 20);
 }
