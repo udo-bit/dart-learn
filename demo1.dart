44a798698b5f968ea9059769732e056b81bb5d44
udo-bit <enote_gxl@163.com>
2024-11-28 22:16:30 +0800

    class

--- demo1.dart
+++ demo1.dart
@@ -1,13 +1,17 @@
 void main() {
-  print(Counter.c);
+  Guitar guitar = Guitar();
+  guitar.tuneInstrument();
 }
 
-class Counter {
-  static bool compate(int a, int b) {
-    return a > b;
+interface class Tuner {
+  void tuneInstrument() {
+    print('Tuning the instrument');
   }
+}
 
-  static int a = 1;
-  static int b = 2;
-  static bool c = compate(a, b);
+class Guitar implements Tuner {
+  @override
+  void tuneInstrument() {
+    print('Tuning the guitar');
+  }
 }
