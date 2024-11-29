eda6eb46d0c5e5e1dc9ee4fc34c6c84841e995d0
udo-bit <enote_gxl@163.com>
2024-11-29 16:26:48 +0800

    dart pub

--- demo1.dart
+++ demo1.dart
@@ -1,12 +1,4 @@
 import 'dart:convert';
 import 'dart:io';
 
-void main() async {
-  var filePath = "tmp.log";
-  var file = File(filePath);
-  var stream = file.openRead();
-  var lines = stream.transform(utf8.decoder).transform(LineSplitter());
-  await for (var line in lines) {
-    print(line);
-  }
-}
+void main() async {}
